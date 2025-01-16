1e52884bfce27c7670cab33b06ff6a281e7a5d25
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2025-01-17 00:20:16 +0530

    added import fix in frontend navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 import { assets } from '../assets/assets';
 import { useContext, useState } from 'react';
-import { ShopContext } from '../context/ShopContext';
+import { ShopContext } from '../context/ShopContext.jsx';
 
 const Navbar = () => {
 	const [visible, setVisible] = useState(false);
